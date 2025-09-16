218a3915783c438231f27871621c987c60956efb
Albinka <a00.ismailova@gmail.com>
2025-09-16 19:08:11 +0600

    app.js

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const modal = document.querySelector('#modal')
 const content = document.querySelector('#content')
 const backdrop = document.querySelector('#backdrop')
 const progress = document.querySelector('#progress')
+const form = document.querySelector('#form')
 
 content.addEventListener('click', openCard)
 backdrop.addEventListener('click', closeModal)
+modal.addEventListener('change', toggleTech)
+form.addEventListener('submit', createTech)
+
+const APP_TITLE = document.title
 
 const technologies = [
     {title: 'HTML', description: 'HTML Text', type: 'html', done: true},
@@ -16,11 +21,44 @@ const technologies = [
 
 
 
-function openCard () {
+function openCard (event) {
+    const data = event.target.dataset
+    const tech = technologies.find(t => t.type === data.type)
+    if (!tech) return  
+    openModal(toModal(tech), tech.title)
+}
+
+function toModal(tech) {
+    const checked = tech.done ? 'checked' : ''
+    return `
+    <h2>${tech.title}</h2>
+        <p>${tech.description}</p>
+        <hr>
+        <div>
+            <input type="checkbox" id="done" ${checked} data-type="${tech.type}">
+            <label for="done">Выучил</label>
+        </div>
+    `
+}
+
+function toggleTech(event) {
+    const type = event.target.dataset.type
+     const tech = technologies.find(t => t.type === type)
+     tech.done = event.target.checked
+
+     init()
+}
+
+
+
+function openModal(html, title = APP_TITLE) {
+    document.title = `${title} | ${APP_TITLE}`
+    modal.innerHTML = html
     modal.classList.add('open')
 }
 
 function closeModal() {
+    document.title = APP_TITLE
     modal.classList.remove('open')
 }
 
@@ -85,10 +123,48 @@ function computeProgressPercent() {
 function toCard(tech) {
     const doneClass = tech.done ? 'done' : ''
     return `
-         <div class="card ${doneClass}">
-            <h3>${tech.title}</h3>
+         <div class="card ${doneClass}" data-type="${tech.type}">
+            <h3 data-type="${tech.type}">${tech.title}</h3>
          </div>
     `
+}
+
+function isInvalid (title, description) {
+    return !title.value || !description.value;
+}
+
+
+function createTech(event) {
+    event.preventDefault()
+   
+
+    const {title, description} = event.target
+
+    if (isInvalid(title, description)) {
+        
+        if (!title.value) title.classList.add('invalid')
+        if (!description.value) description.classList.add('invalid')
+        
+        setTimeout(() => {
+
+            title.classList.remove('invalid')
+            description.classList.remove('invalid')
+        },2000)
+
+        return;
+    }
+
+    const newTech = {
+        title: title.value,
+        description: description.value,
+        done: false,
+        type: title.value.toLowerCase()
+    }
+
+    technologies.push(newTech)
+    title.value = ''
+    description.value = ''
+    init()
 }
 
 init()
